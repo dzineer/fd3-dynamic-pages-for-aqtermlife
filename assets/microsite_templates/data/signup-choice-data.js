114dfcc690959daefefdf77e3d1dcf022b1bd2d4
Frank Decker <frank@dzineer.com>
2017-07-13 10:32:53 -0700

    Added Data Structure for both AQ2E Platform & AQ2E Marketing Platform as JSON data files

---
+++ assets/microsite_templates/data/signup-choice-data.js
@@ -0,0 +1,47 @@
+        // ---------------------- Data ----------------------
+
+        var SignUpChoiceData = {
+
+            'data': {
+
+                'container': jq('#signup-choice-container'),
+                'title': '',
+                'items': [
+
+                    { "element": "div", "state": "signup-choice", "props": { "class": "row" }, "has_children": true, "children": [
+                        { "element": "div", "props": { "class": "col-md-6" }, "has_children": true, "children": [
+
+                            { "element": "div", "props": { "id":"product-one-container", "class": "signup-product product-one-container" }, "has_children": true, "children": [
+
+                                { "element": "div", "props": { "class": "fd3-image-container center-content" }, "has_children": true, "children": [
+                                    { "element": "img", "props": { "class": "signup-image", "src": "/wp-content/plugins/fd3-dynamic-pages/assets/images/page_templates/signup/aq2e-platform-signup-box.png" }, "has_children": false  }
+                                ]},
+
+                                { "element": "div", "props": { "class": "fd3-product-desc-container" }, "has_children": true, "children": [
+                                    { "element": "span", "contents":"Sign me up for the AQ2E Quote Engine Platform for only $20/month", "props": { "class": "fd3-product-desc" }, "has_children": false  }
+                                ]}
+
+                            ]}                           
+
+                        ]},
+
+                        { "element": "div", "props": { "class": "col-md-6" }, "has_children": true, "children": [
+
+                            { "element": "div", "props": { "id":"product-two-container", "class": "signup-product product-two-container" }, "has_children": true, "children": [
+
+                                { "element": "div", "props": { "class": "fd3-image-container center-content" }, "has_children": true, "children": [
+                                    { "element": "img", "props": { "class": "signup-image", "src": "/wp-content/plugins/fd3-dynamic-pages/assets/images/page_templates/signup/aq2e-marketing-platform-signup-box.png" }, "has_children": false  }
+                                ]},
+
+                                { "element": "div", "props": { "class": "fd3-product-desc-container" }, "has_children": true, "children": [
+                                    { "element": "span", "contents":"Yes, sign me up the complete AQ2E Marketing Platform which includes the AQ2E Quote Engine Platform for only $79/month.", "props": { "class": "fd3-product-desc" }, "has_children": false  }
+                                ]}   
+
+                            ]}                        
+                        ]}
+
+                    ]}
+                ]
+
+            }
+        };
